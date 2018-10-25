22a8ffe22a2e982bf40dba50b63dfd5475141c79
neilswalton <neilswalton@gmail.com>
2018-10-25 15:46:52 -0600

    adding the code folder and classifier class file

---
+++ code/classifiers.py
@@ -0,0 +1,15 @@
+################################################################
+#                                                              #
+# The classifier class that implements the general classifer   #
+# superclass, the k-nearest neighbor, Naive Bayes, and         #
+# feedforward neural network classifiers.                      #
+#                                                              #
+# Authors: Amy Peerlinck and Neil Walton                       #
+#                                                              #
+#                                                              #
+################################################################
+
+import numpy as np
+
+class Classifier:
+    pass
