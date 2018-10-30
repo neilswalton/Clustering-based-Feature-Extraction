ebc8a448f08d4667d2c2f6b7e76b9f6d39c96679
amylinck <amy_linck@outlook.com>
2018-10-30 16:55:36 -0600

    deleted unnecessary code from run.py

--- code/run.py
+++ code/run.py
@@ -7,26 +7,6 @@ in_, out = dr.run()
 
 classes = set(out)
 
-
-for i, lab in enumerate(out):
-    if lab==2:
-        out = np.delete(out, i, axis=0)
-        in_ = np.delete(in_, i, axis=0)
-
-'''for c in classes:
-    count = 0
-    for item in out:
-        if item == c:
-            count += 1
-
-    print (c, count)'''
-
-for i, item in enumerate(out):
-    if item == 3:
-        out[i]=2
-    elif item==4:
-        out[i]=3
-
 layers = [4,3]
 
 knn = FFNN(in_, out, layers)
