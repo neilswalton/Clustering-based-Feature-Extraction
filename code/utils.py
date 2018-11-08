cd293b62a576d8e49bd4c1aa41a000b0fbf8f856
neilswalton <neilswalton@gmail.com>
2018-11-07 20:54:01 -0700

    adding skeleton of utils file

---
+++ code/utils.py
@@ -0,0 +1,19 @@
+###################################################
+#                                                 #
+# General utility file for scoring methods, pca,  #
+# etc.                                            #
+#                                                 #
+# Authors: Amy Peerlinck and Neil Walton          #
+#                                                 #
+###################################################
+
+import numpy as np
+
+def pca(data):
+    pass
+
+def silhouette(data, clusters):
+    pass
+
+def f_score(data, clusters):
+    pass
