e1d7ed138b4b2f83a2b5e8f28798deeccb77409f
neil <neilswalton@gmail.com>
2018-12-01 20:48:10 -0700

    automatically calculate hidden layer size, and knn tuning

--- code/generate_results.py
+++ code/generate_results.py
@@ -9,18 +9,19 @@ import time
 import csv
 
 #Set dataset parameters
-dataset = 'nursery'
-num_clusters = 4
-
-#Set parameters for classifiers
-layers = [4] #List of number of nodes per hidden layer
-list_of_ks = [9,10,2,19,19] #If k param needs to change between different extracted features
-
+tune_k = False
+dataset = 'covtype'
+num_clusters = 26
 dr = DataReader('../data/'+dataset+'.csv')
 in_, out = dr.run()
+hidden_size = round((num_clusters + len(set(out)))/2)
+
+#Set parameters for classifiers
+layers = [hidden_size] #List of number of nodes per hidden layer
+list_of_ks = [9,10,2,19,19] #If k param needs to change between different extracted features
 
 #Extract the features
-cpca = ClusterPCA(in_, out, method='kmeans', num_clusters=2, feats_per_cluster=2)
+cpca = ClusterPCA(in_, out, method='kmeans', num_clusters=13, feats_per_cluster=2)
 cpca_feats = cpca.extract_features()
 
 bc = BiclusterExtractor(in_, out, n=num_clusters)
@@ -32,51 +33,67 @@ fc_soft_feats = fc.extract_features()
 fc2 = FeatureCluster(in_, out, method="kmeans", num_clusters=num_clusters, _type="mixed")
 fc_mixed_feats = fc2.extract_features()
 
-features = [in_, cpca_feats, bc_feats, fc_soft_feats, fc_mixed_feats]
-feature_labels = ['All', 'CPCA', 'BC', 'FC_Soft', 'FC_Mixed']
+#Tune k for knn
+if tune_k:
+    all_knn = Knn(in_, out, k=1)
+    cpca_knn = Knn(cpca_feats, out, k=1)
+    bc_knn = Knn(bc_feats, out, k=1)
+    fc_soft_knn = Knn(fc_soft_feats, out, k=1)
+    fc_mixed_knn = Knn(fc_mixed_feats, out, k=1)
 
-file_out_name = dataset + '_results.csv'
-file_out = open(file_out_name, 'w')
-fieldnames = ['method', 'classifier', 'score_type', 'mean', 'stdev', 'time', 'all_scores']
-writer = csv.writer(file_out)
-writer.writerow([num_clusters])
-writer.writerow(fieldnames)
+    all_knn.plot_k_scores(20)
+    cpca_knn.plot_k_scores(20)
+    bc_knn.plot_k_scores(20)
+    fc_soft_knn.plot_k_scores(20)
+    fc_mixed_knn.plot_k_scores(20)
 
-for i, feats in enumerate(features):
-    k = list_of_ks[i]
-    method = feature_labels[i]
-    knn = Knn(feats, out, k)
-    ffnn = FFNN(in_, out, layers)
-    nb = NaiveBayes(in_, out)
+else:
 
-    knn_start = time.time()
-    k_acc_mean, k_acc_std, k_accs, k_f_mean, k_f_std, k_fs = knn.k_fold_score(10, 'both')
-    knn_stop = time.time()
-    ffnn_start = time.time()
-    f_acc_mean, f_acc_std, f_accs, f_f_mean, f_f_std, f_fs = ffnn.k_fold_score(10, 'both')
-    ffnn_stop = time.time()
-    nb_start = time.time()
-    n_acc_mean, n_acc_std, n_accs, n_f_mean, n_f_std, n_fs = nb.k_fold_score(10, 'both')
-    nb_stop = time.time()
+    features = [in_, cpca_feats, bc_feats, fc_soft_feats, fc_mixed_feats]
+    feature_labels = ['All', 'CPCA', 'BC', 'FC_Soft', 'FC_Mixed']
 
-    knn_time = knn_stop - knn_start
-    ffnn_time = ffnn_stop - ffnn_start
-    nb_time = nb_stop - nb_start
+    file_out_name = dataset + '_results.csv'
+    file_out = open(file_out_name, 'w')
+    fieldnames = ['method', 'classifier', 'score_type', 'mean', 'stdev', 'time', 'all_scores']
+    writer = csv.writer(file_out)
+    writer.writerow([num_clusters])
+    writer.writerow(fieldnames)
 
-    writer.writerow([method, 'knn', 'accuracy', k_acc_mean,
-        k_acc_std, knn_time, k_accs])
-    writer.writerow([method, 'knn', 'fscore', k_f_mean,
-        k_f_std, knn_time, k_fs])
+    for i, feats in enumerate(features):
+        k = list_of_ks[i]
+        method = feature_labels[i]
+        knn = Knn(feats, out, k)
+        ffnn = FFNN(in_, out, layers)
+        nb = NaiveBayes(in_, out)
 
-    writer.writerow([method, 'ffnn', 'accuracy', f_acc_mean,
-        f_acc_std, ffnn_time, f_accs])
-    writer.writerow([method, 'ffnn', 'fscore', f_f_mean,
-        f_f_std, ffnn_time, f_fs])
+        knn_start = time.time()
+        k_acc_mean, k_acc_std, k_accs, k_f_mean, k_f_std, k_fs = knn.k_fold_score(10, 'both')
+        knn_stop = time.time()
+        ffnn_start = time.time()
+        f_acc_mean, f_acc_std, f_accs, f_f_mean, f_f_std, f_fs = ffnn.k_fold_score(10, 'both')
+        ffnn_stop = time.time()
+        nb_start = time.time()
+        n_acc_mean, n_acc_std, n_accs, n_f_mean, n_f_std, n_fs = nb.k_fold_score(10, 'both')
+        nb_stop = time.time()
 
-    writer.writerow([method, 'nb', 'accuracy', n_acc_mean,
-        n_acc_std, nb_time, n_accs])
-    writer.writerow([method, 'nb', 'fscore', n_f_mean,
-        n_f_std, nb_time, n_fs])
+        knn_time = knn_stop - knn_start
+        ffnn_time = ffnn_stop - ffnn_start
+        nb_time = nb_stop - nb_start
+
+        writer.writerow([method, 'knn', 'accuracy', k_acc_mean,
+            k_acc_std, knn_time, k_accs])
+        writer.writerow([method, 'knn', 'fscore', k_f_mean,
+            k_f_std, knn_time, k_fs])
+
+        writer.writerow([method, 'ffnn', 'accuracy', f_acc_mean,
+            f_acc_std, ffnn_time, f_accs])
+        writer.writerow([method, 'ffnn', 'fscore', f_f_mean,
+            f_f_std, ffnn_time, f_fs])
+
+        writer.writerow([method, 'nb', 'accuracy', n_acc_mean,
+            n_acc_std, nb_time, n_accs])
+        writer.writerow([method, 'nb', 'fscore', n_f_mean,
+            n_f_std, nb_time, n_fs])
 
 
 
